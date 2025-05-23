25eb933a32e3e769d562f77bb2b1da12caf7ff82
Pawel Gurlo <pawel.gurlo@gmail.com>
2025-05-23 14:28:52 +0200

    Add complete RPS funcitonality and link with GUI

--- javascript/main.js
+++ javascript/main.js
@@ -11,87 +11,108 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    return prompt("Play your move: ");
-}
-
-function playRound(human_choice, computer_choice) {
-    human_choice = human_choice.toLowerCase();
-
-    console.log("Your choice: " + human_choice);
-    console.log("Computer choice: " + computer_choice);
-
+function getGameResult(human_choice, computer_choice) {
     switch (human_choice) {
         case "rock":
             if (computer_choice == "rock") {
-                console.log("Tie!");
-                return 2;
+                return "tie";
             }
             else if (computer_choice == "scissors") {
-                console.log("You won!");
-                return 0
+                return "player win";
             }
             else {
-                console.log("You lost!");
-                return 1;
+                return "computer win";
             }
             break;
         case "paper":
             if (computer_choice == "paper") {
-                console.log("Tie!");
-                return 2;
+                return "tie";
             }
             else if (computer_choice == "rock") {
-                console.log("You won!");
-                return 0
+                return "player win";
             }
             else {
-                console.log("You lost!");
-                return 1;
+                return "computer win";
             }
             break;
         case "scissors":
             if (computer_choice == "scissors") {
-                console.log("Tie!");
-                return 2;
+                return "tie";
             }
             else if (computer_choice == "paper") {
-                console.log("You won!");
-                return 0
+                return "player win";
             }
             else {
-                console.log("You lost!");
-                return 1;
+                return "computer win";
             }
             break;
     }
 }
 
-function playGame() {
-    let human_score = 0;
-    let computer_score = 0;
-    for (let i=0; i<5; i++) {
-        output = playRound(prompt("Play your move: "), getComputerChoice());
+function printWinner() {
+    winner_label.textContent = (player_score == BEST_OF) ? "Player Won!" : "Computer Won!";
+}
 
-        // human won
-        if (output == 0) {
-            human_score++;
-        }
-        // comp won
-        else if (output == 1) {
+function reset() {
+    player_score = 0;
+    computer_score = 0;
+    winner_label.textContent = "";
+    result_label.textContent = "";
+    updateScores();
+    game_ended = false;
+}
+
+function showResult(player_choice, computer_choice, result) {
+    result_label.textContent = `${player_choice} : ${computer_choice} -> ${result}`;
+}
+
+function updateScores() {
+    player_score_label.textContent = `Player: ${player_score}`;
+    computer_score_label.textContent = `Computer: ${computer_score}`;
+}
+
+function playRound(player_choice) {
+    let computer_choice = getComputerChoice();
+    let result = getGameResult(player_choice, computer_choice);
+
+    switch (result) {
+        case "player win":
+            player_score++;
+            break;
+        case "computer win":
             computer_score++;
-        }
-    
-        if (human_score > computer_score) {
-            console.log("Human Player Wins!");
-        }
-        else if (computer_score > human_score) {
-            console.log("Computer Wins!");
-        }
-        else {
-            console.log("Tie!");
-        }
+            break;
+        case "tie":
+            break;
+    }
+
+    showResult(player_choice, computer_choice, result);
+    updateScores();
+
+    // finished round
+    if (player_score >= BEST_OF || computer_score >= BEST_OF) {
+        game_ended = true;
+        printWinner();
     }
 }
 
-playGame();
+
+const btn_container = document.querySelector(".buttons");
+const winner_label = document.querySelector("#winner-label");
+const reset_btn = document.querySelector("#reset");
+const result_label = document.querySelector("#game-result")
+const player_score_label = document.querySelector("#player-score");
+const computer_score_label = document.querySelector("#computer-score");
+const BEST_OF = 5
+let player_score = 0;
+let computer_score = 0;
+let game_ended = false;
+
+// event delegation
+btn_container.addEventListener('click', e => {
+        if (!game_ended) playRound(e.target.id);
+    });
+
+reset_btn.addEventListener('click', e => {
+    reset();
+})
